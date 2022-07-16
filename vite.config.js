86f01dbb6fb7a64a8d924521fb7195d053af6495
Patrick Sheron Moucle <mouclepatrick@protonmail.com>
2022-07-16 10:50:25 +0000

    revert change

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	server: {
-        hmr: {
-          port: process.env.HMR_HOST ? 5173 : 3000,
-          host: process.env.HMR_HOST
-            ? process.env.HMR_HOST.substring('https://'.length)
-            : 'localhost',
-        },
-      },
+  plugins: [sveltekit()],
 };
 
 export default config;
