8314cf38e88e3e8efb6c3a58077457f5ed936833
Patrick Sheron Moucle <mouclepatrick@protonmail.com>
2022-07-16 10:30:35 +0000

    Configure HMR on gitpod and  localhost

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,21 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    vite: {
+      server: {
+        hmr: {
+          clientport:process.env.HMR_HOST ? 5173 : 3000,
+          host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : process.env.'localhost',
+        },
+      },
+    },
+  },
 };
 
 export default config;
