82c56931845e16851efd138aff608df986e4c952
Patrick Sheron Moucle <mouclepatrick@protonmail.com>
2022-07-16 12:47:01 +0000

    Develop HTML  structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+  <form action="" method="">
+    <input type="hidden" name="done" value="" />
+    <button aria-label="Mark done/not done">Done/not done</button>
+  </form>
+  <form>
+    <input type="text" />
+    <button aria-label="Save todo">Save</button>
+  </form>
+  <form>
+    <button aria-label="Delete todo">Delete</button>
+  </form>
+</div>
+
+<style></style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import TodoItem from '$lib/todo-item.svelte';
+  const title = 'Todo';
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+<h1>{title}</h1>
+
+<form action="" method="post">
+  <input
+    type="text"
+    name="todo"
+    placeholder="+ tap to add a todo"
+    aria-label="Add a todo"
+  />
+</form>
+<section>
+  <TodoItem />
+</section>
